897991e0e617c035d8d353c8be060796c9205472
deknowmerchant <deknowmerchant@gmail.com>
2023-08-11 12:26:44 -0700

    number

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: This program prints all single digit numbers of base 10
- *              starting from 0, followed by a new line, using the putchar function.
+ * main - Entry point, prints single-digit numbers of base 10 followed by a new line
  *
  * Return: Always 0 (Success)
  */
