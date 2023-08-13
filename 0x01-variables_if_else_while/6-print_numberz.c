afb85364717b8f44232616db91595f08aadca6ee
deknowmerchant <deknowmerchant@gmail.com>
2023-08-12 18:24:23 -0700

    single number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    /* Print all single digit numbers of base 10 starting from 0, followed by a new line. */
+	int c;
 
-    int c;
-    c = 0;
-    while (c < 10) {
-        putchar(c + '0');
-        c++;
-    }
-    putchar('\n');
-    return (0);
+	c = 0;
+	while (c < 10)
+	{
+		putchar(c + '0');
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
