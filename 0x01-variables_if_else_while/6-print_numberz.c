5a36c7d4dbd480ab6445be87c5974c4a1198ddc6
deknowmerchant <deknowmerchant@gmail.com>
2023-08-12 18:17:04 -0700

    single number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,12 +2,14 @@
 
 int main(void)
 {
-    int c;
-    c = 0;
-    while (c < 10) {
-        putchar(c + '0');
-        c++;
-    }
-    putchar('\n');
-    return 0;
+	// Print all single digit numbers of base 10 starting from 0, followed by a new line.
+	
+	int c;
+	c = 0;
+	while (c < 10) {
+		putchar(c + '0');
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
