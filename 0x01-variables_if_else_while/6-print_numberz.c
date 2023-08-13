eb7347f1de31d1f9c64a8a26d6c264d735dc1cf4
deknowmerchant <deknowmerchant@gmail.com>
2023-08-12 18:13:07 -0700

    same digit number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,41 +1,13 @@
 #include <stdio.h>
 
-/**
- * main - Entry point, prints single-digit numbers of base 10 followed by a new line
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-    putchar('0' + 0);
-    putchar(',');
-    putchar(' ');
-    putchar('0' + 1);
-    putchar(',');
-    putchar(' ');
-    putchar('0' + 2);
-    putchar(',');
-    putchar(' ');
-    putchar('0' + 3);
-    putchar(',');
-    putchar(' ');
-    putchar('0' + 4);
-    putchar(',');
-    putchar(' ');
-    putchar('0' + 5);
-    putchar(',');
-    putchar(' ');
-    putchar('0' + 6);
-    putchar(',');
-    putchar(' ');
-    putchar('0' + 7);
-    putchar(',');
-    putchar(' ');
-    putchar('0' + 8);
-    putchar(',');
-    putchar(' ');
-    putchar('0' + 9);
+    int c;
+    c = 0;
+    while (c < 10) {
+        putchar(c + '0');
+        c++;
+    }
     putchar('\n');
-
-    return (0);
+    return 0;
 }
