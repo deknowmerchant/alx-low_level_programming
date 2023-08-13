5a95302a58c1590e7bebf37637c9f482381751fd
deknowmerchant <deknowmerchant@gmail.com>
2023-08-12 18:42:51 -0700

    numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all numbers of base 16 in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char c;
+
+    for (c = '0'; c <= '9'; c++)
+        putchar(c);
+    for (c = 'a'; c <= 'f'; c++)
+        putchar(c);
+    putchar('\n');
+
+    return (0);
+}
