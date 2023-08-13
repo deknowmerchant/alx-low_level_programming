c0dd71dfb837ab7b3129df41aa1d70321b129eb6
deknowmerchant <deknowmerchant@gmail.com>
2023-08-12 19:38:00 -0700

    combination of two numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,21 +15,21 @@ int main(void)
     {
         for (num2 = num1 + 1; num2 <= 99; num2++)
         {
-            putchar((num1 / 10) + '0');
-            putchar((num1 % 10) + '0');
-            putchar(' ');
-            putchar((num2 / 10) + '0');
-            putchar((num2 % 10) + '0');
+            putchar((num1 / 10) + 0);
+            putchar((num1 % 10) + 0);
+            putchar( );
+            putchar((num2 / 10) + 0);
+            putchar((num2 % 10) + 0);
 
             if (num1 != 98 || num2 != 99)
             {
-                putchar(',');
-                putchar(' ');
+                putchar(,);
+                putchar( );
             }
         }
     }
 
-    putchar('\n');
+    putchar(n);
 
     return (0);
 }
