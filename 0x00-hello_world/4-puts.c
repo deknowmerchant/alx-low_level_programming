7aea7907ff698e83963d757fcc1cff8ae23ff049
deknowmerchant <deknowmerchant@gmail.com>
2023-08-14 17:53:31 -0700

    assembler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the specified string followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle,");
-    return 0;
+    return (0);
 }
