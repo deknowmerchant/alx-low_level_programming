b70026daec41f119904f329db448af03a674dd61
deknowmerchant <deknowmerchant@gmail.com>
2023-08-14 18:32:34 -0700

    prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,12 +3,16 @@
 /**
  * main - Entry point
  *
- * Description: Prints a specified string with proper grammar, followed by a new line.
+ * Description: Prints the size of various types on the computer it is compiled and run on.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
