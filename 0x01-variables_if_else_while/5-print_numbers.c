d328086a325b7bc5755f31113de3dbf74cc84c0b
deknowmerchant <deknowmerchant@gmail.com>
2023-08-14 19:09:07 -0700

    print

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point, prints single-digit numbers of base 10 followed by a new line
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int digit = 0;
+    int digit = 0;
 
-while (digit <= 9)
-{
-putchar(digit + '0');
-digit++;
-}
+    while (digit <= 9)
+    {
+        putchar(digit + '0');
+        digit++;
+    }
 
-putchar('\n');
+    putchar('\n');
 
-return (0);
+    return (0);
 }
