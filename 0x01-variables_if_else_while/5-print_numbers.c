000761a52518b756d0ee6c9c50284e93c7b9da08
deknowmerchant <deknowmerchant@gmail.com>
2023-08-14 19:12:41 -0700

    print

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,15 +7,12 @@
  */
 int main(void)
 {
-    int digit = 0;
-
-    while (digit <= 9)
-    {
-        putchar(digit + '0');
-        digit++;
-    }
-
-    putchar('\n');
-
-    return (0);
+	int digit = 0;
+	while (digit <= 9)
+}
+	putchar(digit + '0');
+	digit++;
+	}
+	putchar('\n');
+	return (0);
 }
