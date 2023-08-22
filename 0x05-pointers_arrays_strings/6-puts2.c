c79f27675e7d6bf43cb8f808f60164537a74da44
deknowmerchant <deknowmerchant@gmail.com>
2023-08-21 19:59:52 -0700

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string, 
+ * puts2 - Prints every character of a string
  * starting with the first character.
  * @str: Pointer to the string.
  *
