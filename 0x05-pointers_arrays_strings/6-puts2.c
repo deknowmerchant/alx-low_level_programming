7f215dd6ccf5052ceda747e9ecb38dc0648b2f0d
deknowmerchant <deknowmerchant@gmail.com>
2023-08-21 19:36:56 -0700

    pointer task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string starting with the first.
+ * puts2 - Prints every other character of a string, starting with the first character.
  * @str: Pointer to the string.
  *
  * Return: None.
@@ -15,5 +15,6 @@ void puts2(char *str)
 		_putchar(str[i]);
 		i += 2;
 	}
+
 	_putchar('\n');
 }
