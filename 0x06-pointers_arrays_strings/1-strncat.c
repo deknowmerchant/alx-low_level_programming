ff64a92d5883be3a4204e5701b85e45034658125
deknowmerchant <deknowmerchant@gmail.com>
2023-08-24 13:28:01 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings up to n bytes.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of bytes from src to concatenate.
+ * 
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *result = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	*dest = '\0';
+
+	return result;
+}
