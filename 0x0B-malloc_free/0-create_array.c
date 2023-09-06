0852258dd065e163108ba1e5df7d5da20103607d
deknow <deknow@kali.kali>
2023-09-05 22:53:42 -0400

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,12 +3,11 @@
 #include <stddef.h>
 
 /**
- *Create an array of chars and initialize with a specific char.
+ * create_array - Create an array of chars and initialize with a specific char.
  * @size: Size of the array to create.
  * @c: Character to initialize the array elements.
  *
- * Return: Pointer to the created array,
- * or NULL if size is 0 or memory allocation fails.
+ * Return: Pointer to the created array, or NULL if size is 0 or memory allocation fails.
  */
 char *create_array(unsigned int size, char c)
 {
@@ -21,3 +20,4 @@ char *create_array(unsigned int size, char c)
 		str[i] = c;
 	return (str);
 }
+
