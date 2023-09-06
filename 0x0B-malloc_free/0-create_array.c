c20832d1e87bf39d614d6a26643e633faafc6eb8
deknow <deknow@kali.kali>
2023-09-05 21:44:16 -0400

    malloc task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "main.h" /* Include the header file */
 
 /**
@@ -10,13 +9,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *str;
-	unsigned int i;
+        char *str;
+        unsigned int i;
 
-	str = malloc(sizeof(char) *size);
-	if (size ==0 ||str == NULL)
-		return (NULL);
-	for (i =0; i < size; i++)
-		str[i] -c;
-	return (str);
+        str = malloc(sizeof(char) * size);
+        if (size == 0 || str == NULL)
+                return (NULL);
+        for (i = 0; i < size; i++)
+                str[i] = c;
+        return (str);
 }
+
