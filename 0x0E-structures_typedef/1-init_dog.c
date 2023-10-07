b46a70af95edeee084fc1883278898ee15b6d784
deknow <deknow@kali.kali>
2023-10-07 12:46:16 -0400

    init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,23 +1,20 @@
-#include <stdio.h>
-#include "dog.h"
+#include <stdlib.h>
+	#include "dog.h"
 
-/**
- * init_dog - Initializes a struct dog.
- * @d: A pointer to the struct dog to initialize.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner's name of the dog.
- *
- * Description: This function initializes a struct dog with the provided
- * name, age, and owner's name.
- */
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
 
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
+	/**
+	 * init_dog - initializes a variable of type struct dog
+	 * @d: pointer to struct dog to initialize
+	 * @name: name to initialize
+	 * @age: age to initialize
+	 * @owner: owner to initialize
+	 */
+	void init_dog(struct dog *d, char *name, float age, char *owner)
+	{
+		if (d == NULL)
+			d = malloc(sizeof(struct dog));
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 
